0936e7a815b33ef17139609a64e93d395b1d8dbf
Dana He <danahe97@fb.com>
2017-06-28 17:05:08 -0700

    added progress bar

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -17,13 +18,17 @@ import cz.msebera.android.httpclient.Header;
 
 public class ComposeActivity extends AppCompatActivity {
 
+    ProgressBar pb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+        pb = (ProgressBar) findViewById(R.id.pbLoading);
     }
 
     public void post(View view) {
+        pb.setVisibility(ProgressBar.VISIBLE);
         TwitterClient client = new TwitterClient(this);
         final EditText newTweet = (EditText) findViewById(R.id.etNewTweet);
         String newTweetString = newTweet.getText().toString();
@@ -38,8 +43,10 @@ public class ComposeActivity extends AppCompatActivity {
                     data.putExtra("tweet", Parcels.wrap(tweet));
                     // Activity finished ok, return the data
                     setResult(RESULT_OK, data); // set result code and bundle data for response
+                    pb.setVisibility(ProgressBar.INVISIBLE);
                     finish(); // closes the activity, pass data to parent
                 } catch (JSONException e) {
+                    pb.setVisibility(ProgressBar.INVISIBLE);
                     e.printStackTrace();
                 }
             }
